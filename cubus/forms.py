40f76833e429ae255e5a455245db911c2c158a56
mattiazanichelli <mattia.zanichelli@student.supsi.ch>
2022-08-23 15:18:31 +0200

    removed commented code

--- cubus/forms.py
+++ cubus/forms.py
@@ -1,16 +1,1 @@
-# from django import forms
-# from django.contrib.auth.forms import UserCreationForm
-# from django.contrib.auth.models import User
-# from .models import User
-#
-#
-# class RegisterForm(UserCreationForm):
-#     first_name = forms.CharField()
-#     last_name = forms.CharField()
-#     email = forms.CharField()
-#     password1 = forms.PasswordInput()
-#     password2 = forms.PasswordInput()
-#
-#     class Meta:
-#         model = User
-#         fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
+
--- cubus/models.py
+++ cubus/models.py
@@ -1,8 +1,3 @@
-# import uuid
-# from django.db import models
-# from django.contrib.auth.models import AbstractUser
-# from typing import Optional
-# from pydantic import Field
 from enum import Enum
 
 
@@ -10,24 +5,4 @@ class OsType(Enum):
     INTRUSION_DETECTION = 1
     NETWORK_MONITORING = 2
 
-# class ISO:
-#
-#     def __init__(self):
-#         pass
-#
-#     # id: str = Field(default_factory=uuid.uuid4, alias="_id")
-#     name: str = Field(...)
-#     os_type: OsType = Field(...)
-#     packages: Optional[list]
-#     include_docker_images: bool = Field(...)
-#     docker_images: Optional[list]
-
-
-# class User:
-#
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#     email = models.CharField(max_length=100)
-#     password: models.CharField(max_length=100)
-#     ISOs: Optional[list]
 
